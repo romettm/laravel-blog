03610f9d11a5c8e5ee4727a4917be4f0ab04e28c
Romet Tiikmaa <romet@planet.ee>
2020-08-26 21:48:20 +0300

    bug fix: class name

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,17 @@ Route::post('comment/store', 'commentController@store')->name('comment.store');
 
 Route::group(['middleware' => 'auth'], function() {
 
-	Route::get('post-list', 'postController@list')->name('post.list');
-	Route::get('post-new', 'postController@form')->name('post.new');
-	Route::get('post-edit/{slug}', 'postController@form')->name('post.edit');
-	Route::get('post-delete/{slug}', 'postController@delete')->name('post.delete');
-	Route::post('post-submit', 'postController@store')->name('post.store');
+	Route::get('post-list', 'PostController@list')->name('post.list');
+	Route::get('post-new', 'PostController@form')->name('post.new');
+	Route::get('post-edit/{slug}', 'PostController@form')->name('post.edit');
+	Route::get('post-delete/{slug}', 'PostController@delete')->name('post.delete');
+	Route::post('post-submit', 'PostController@store')->name('post.store');
 
-	Route::get('user-list', 'userController@list')->name('user.list');
-	Route::get('user-new', 'userController@form')->name('user.new');
-	Route::get('user-edit/{slug}', 'userController@form')->name('user.edit');
-	Route::get('user-delete/{slug}', 'userController@delete')->name('user.delete');
-	Route::post('user-submit', 'userController@store')->name('user.store');
+	Route::get('user-list', 'UserController@list')->name('user.list');
+	Route::get('user-new', 'UserController@form')->name('user.new');
+	Route::get('user-edit/{slug}', 'UserController@form')->name('user.edit');
+	Route::get('user-delete/{slug}', 'UserController@delete')->name('user.delete');
+	Route::post('user-submit', 'UserController@store')->name('user.store');
 	
 });
 
